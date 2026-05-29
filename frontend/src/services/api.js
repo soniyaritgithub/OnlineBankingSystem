d96adf981a9765e685bdfbe05f2496fd94754c7b
sunidhi shinde <sunidhishinde28@gmail.com>
2026-05-29 15:37:59 +0530

    fixed api url for django backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,11 @@ import axios from "axios";
 
 const API = axios.create({
 
-  baseURL: "https://onlinebankingsystem-qguw.onrender.com/api",
+  baseURL: "https://smartbank-django-backend.onrender.com/api",
+
+  headers: {
+    "Content-Type": "application/json",
+  },
 
 });
 
